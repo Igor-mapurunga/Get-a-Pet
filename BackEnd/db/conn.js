57bf89260f0c2184cd48d00356588015369f19ff
Igor <mapurungaigor@gmail.com>
2025-12-04 16:58:31 -0300

    feat: dependencia modificada

--- BackEnd/db/conn.js
+++ BackEnd/db/conn.js
@@ -1,22 +1,9 @@
-const { Pool } = require('pg')
+const { Sequelize } = require('sequelize')
 
-async function main () {
-  // 1. Criar pool de conexões
-  const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'GetAPet',
-    password: '123@',
-    port: 5432
-  })
+const sequelize = new Sequelize('GetAPet', 'postgres', '123@', {
+  host: 'localhost',
+  dialect: 'postgres',
+  logging: false
+})
 
-    try {
-    // 2. Testar a conexão
-    const result = await pool.query('SELECT NOW()')
-    console.log('Conectado! Hora atual do banco:', result.rows[0])
-  } catch (err) {
-    console.error('Erro ao conectar no PostgreSQL:', err)
-  }
-}
-
-main()
+module.exports = sequelize
