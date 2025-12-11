30108310330a922840efd0978dfb1dc9e0372e38
Igor <mapurungaigor@gmail.com>
2025-12-11 10:33:21 -0300

    feat: criado o arquivo PetRoutes

---
+++ BackEnd/routes/PetRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router()
+const PetController = require('../controllers/PetController')
+const verifyToken = require('../helpers/verify-token')
+
+router.post('/create',verifyToken, PetController.create)
+
+module.exports = router
